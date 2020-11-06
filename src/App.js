00a22466d1ab339f2b3e71fb1cd1aaf0dbf4d8f7
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-11-06 16:04:28 +0100

    12/16

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
   }
 
   const putNumber = (newNumber) =>{
+    setWynik('')
     if(newNumber == '+' || newNumber == '-' || newNumber == '/' || newNumber =='*' || number == '+' || number == '-' || number == '/' || number =='*'){
       setNumber(newNumber)
       oldNumber ? setOldNumber(oldNumber + `${newNumber}`) : setOldNumber(newNumber)
@@ -41,32 +42,41 @@ function App() {
 //eval - zsumuje stringa :D
   const operations = (operator) => {
     setNumber(operator)
-    setOldNumber(oldNumber + `${operator}`)
+    if(oldNumber.length == 0){
+      return;
+    }else if(oldNumber[oldNumber.length-1] == '+'|| oldNumber[oldNumber.length-1] == '*' || oldNumber[oldNumber.length-1] == '/' ||oldNumber[oldNumber.length-1] == '-'){
+      setOldNumber(oldNumber.slice(0,oldNumber-1) + operator)
+    }else{
+      setOldNumber(oldNumber + `${operator}`)
+    }
   }
 
   const calculate = () => {
-    setOldNumber('')
-    setWynik(eval(oldNumber))
+    /*setWynik(eval(oldNumber))
     console.log(wynik)
+    setOldNumber(oldNumber + ' = ' + wynik) */
+    setWynik(eval(oldNumber))
+    setOldNumber(oldNumber + ' = ' + wynik)
+    console.log(wynik)
+    
   }
   return (
     <div id="calculator" className="calc">
       <div className="upper-displayer">
-      <div>{wynik}</div>
       <div>{oldNumber}</div>
       </div>
-      <div id="display">{number}</div>
+      <div id="display">{wynik ? wynik : number}</div>
       <Button idx="clear" name="AC" size="2" onClick={clear}/>
-      <Button idx="divide" name="/" size="1" onClick={() => putNumber('/')}/>
-      <Button idx="multiply" name="x" size="1" onClick={() => putNumber('*')}/>
+      <Button idx="divide" name="/" size="1" onClick={() => operations('/')}/>
+      <Button idx="multiply" name="x" size="1" onClick={() => operations('*')}/>
       <Button idx="seven" name="7" size="1" onClick={() => putNumber(7)}/>
       <Button idx="eight" name="8" size="1" onClick={() => putNumber(8)}/>
       <Button idx="nine" name="9" size="1" onClick={() => putNumber(9)}/>
-      <Button idx="subtract" name="-" size="1" onClick={() => putNumber('-')}/>
+      <Button idx="subtract" name="-" size="1" onClick={() => operations('-')}/>
       <Button idx="four" name="4" size="1" onClick={() => putNumber(4)}/>
       <Button idx="five" name="5" size="1" onClick={() => putNumber(5)}/>
       <Button idx="six" name="6" size="1" onClick={() => putNumber(6)}/>
-      <Button idx="add" name="+" size="1" onClick={() => putNumber('+')}/>
+      <Button idx="add" name="+" size="1" onClick={() => operations('+')}/>
       <Button idx="one" name="1" size="1" onClick={() => putNumber(1)}/>
       <Button idx="two" name="2" size="1" onClick={() => putNumber(2)}/>
       <Button idx="three" name="3" size="1" onClick={() => putNumber(3)}/>
